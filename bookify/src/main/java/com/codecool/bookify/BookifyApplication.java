07de77c78941b3b809e5167ee9220870d127f6e8
Tomek K <tomek.krogulec@o2.pl>
2020-12-29 18:50:12 +0100

    Initial commit

---
+++ bookify/src/main/java/com/codecool/bookify/BookifyApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.bookify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookifyApplication.class, args);
+	}
+
+}
---
+++ bookify/src/test/java/com/codecool/bookify/BookifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.bookify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
