cd361ed9ca05716624fc5f18e4361f3856600cc6
Tomek K <tomek.krogulec@o2.pl>
2021-01-04 13:40:00 +0100

    add Role.java

---
+++ bookify/src/main/java/com/codecool/bookify/Model/Role.java
@@ -0,0 +1,32 @@
+package com.codecool.bookify.Model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users = new HashSet<>();
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public enum UserRoleEnum {
+        ADMIN, USER, EMPLOYEE, OWNER
+    }
+}
