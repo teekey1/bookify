45122e73f07d0efb185f03a3235b2736256c70f3
Tomek K <tomek.krogulec@o2.pl>
2021-01-14 19:28:19 +0100

    changed from Date to Timestamp and added @JsonFormat pattern

--- bookify/src/main/java/com/codecool/bookify/model/Appointment.java
+++ bookify/src/main/java/com/codecool/bookify/model/Appointment.java
@@ -1,11 +1,12 @@
-package com.codecool.bookify.Model;
+package com.codecool.bookify.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.Date;
+import java.sql.Timestamp;
 
 @Entity
 @Table(name = "appointments")
@@ -18,22 +19,24 @@ public class Appointment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false)
-    private Date start_datetime;
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm")
+    private Timestamp start_datetime;
     @Column(nullable = false)
-    private Date end_datetime;
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm")
+    private Timestamp end_datetime;
     @Column(nullable = false)
     private int price;
-    @JoinColumn(name = "client_id", nullable = false)
+    @JoinColumn(name = "client_id", nullable = false, referencedColumnName = "id")
     @ManyToOne
     private User client;
-    @JoinColumn (name = "employee_id", nullable = false)
+    @JoinColumn (name = "employee_id", nullable = false, referencedColumnName = "id")
     @ManyToOne
     private User employee;
     @JoinColumn (name = "company_detail_id", nullable = false)
     @ManyToOne
     private CompanyDetail companyDetail;
 
-    public Appointment(Date start_datetime, Date end_datetime, int price, User client, User employee, CompanyDetail companyDetail) {
+    public Appointment(Timestamp start_datetime, Timestamp end_datetime, int price, User client, User employee, CompanyDetail companyDetail) {
         this.start_datetime = start_datetime;
         this.end_datetime = end_datetime;
         this.price = price;
