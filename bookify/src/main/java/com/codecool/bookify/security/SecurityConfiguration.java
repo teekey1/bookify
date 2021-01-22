4f0f807a264afb04e514187aa6702ac96d379700
Tomek K <tomek.krogulec@o2.pl>
2021-01-22 08:55:41 +0100

    changes in SecurityConfiguration.java

--- bookify/src/main/java/com/codecool/bookify/security/SecurityConfiguration.java
+++ bookify/src/main/java/com/codecool/bookify/security/SecurityConfiguration.java
@@ -1,45 +1,86 @@
 package com.codecool.bookify.security;
 
-import com.codecool.bookify.service.MyUserDetailsService;
-import lombok.RequiredArgsConstructor;
+import com.codecool.bookify.auth.ApplicationUserService;
+import com.codecool.bookify.jwt.JwtConfig;
+//import com.codecool.bookify.jwt.JwtTokenVerifier;
+import com.codecool.bookify.jwt.JwtUsernameAndPasswordAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import javax.crypto.SecretKey;
+import java.util.Arrays;
+import java.util.Collections;
 
+import static com.codecool.bookify.security.ApplicationUserRole.*;
+
+@Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    private final PasswordEncoder passwordEncoder;
+    private final ApplicationUserService applicationUserService;
+    private final SecretKey secretKey;
+    private final JwtConfig jwtConfig;
+
+//    @Autowired
+//    MyUserDetailsService myUserDetailsService;
+
     @Autowired
-    MyUserDetailsService myUserDetailsService;
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(myUserDetailsService);
-
+    public SecurityConfiguration(PasswordEncoder passwordEncoder, ApplicationUserService applicationUserService, SecretKey secretKey, JwtConfig jwtConfig) {
+        this.passwordEncoder = passwordEncoder;
+        this.applicationUserService = applicationUserService;
+        this.secretKey = secretKey;
+        this.jwtConfig = jwtConfig;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/user").hasRole("USER")
-                .antMatchers("/admin").hasAnyRole("ADMIN", "USER")
-                .antMatchers("/owner").hasRole("OWNER")
+        http
+                .cors().and()
+                .csrf().disable()
+                .formLogin().and()
+//                .sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+                .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager(), jwtConfig, secretKey))
+//                .addFilterAfter(new JwtTokenVerifier(secretKey, jwtConfig), JwtUsernameAndPasswordAuthenticationFilter.class)
+                .authorizeRequests()
+//                .antMatchers("/css/**", "/js/**", "/register", "/index").permitAll()
+//                .antMatchers("/admin").hasRole(ADMIN.name())
                 .antMatchers("/").permitAll()
-                .and().formLogin();
-        http.csrf().disable();
+                .anyRequest()
+                .authenticated();
+//                .and().formLogin();
+
 
     }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
     @Bean
-    public PasswordEncoder getPasswordEncoder() {
-        return new BCryptPasswordEncoder();
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(applicationUserService);
+        return provider;
     }
 }
 
