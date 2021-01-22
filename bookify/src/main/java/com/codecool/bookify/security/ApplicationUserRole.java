535c8404257abff6420c04b36400875209c7c2f2
Tomek K <tomek.krogulec@o2.pl>
2021-01-22 08:54:24 +0100

    added ApplicationUserRole.java

---
+++ bookify/src/main/java/com/codecool/bookify/security/ApplicationUserRole.java
@@ -0,0 +1,45 @@
+package com.codecool.bookify.security;
+
+import com.google.common.collect.Sets;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static com.codecool.bookify.security.ApplicationUserPermission.*;
+
+public enum ApplicationUserRole {
+    USER(Sets.newHashSet()),
+    ADMIN(Sets.newHashSet(APPOINTMENT_READ,
+            APPOINTMENT_WRITE,
+            CATEGORY_READ,
+            CATEGORY_WRITE,
+            COMPANY_READ,
+            COMPANY_WRITE,
+            COMPANY_DETAIL_READ,
+            COMPANY_DETAIL_WRITE,
+            ROLE_READ,
+            ROLE_WRITE,
+            USER_READ,
+            USER_WRITE,
+            USER_RATE_READ,
+            USER_RATE_WRITE));
+
+    private final Set<ApplicationUserPermission> permissions;
+
+    ApplicationUserRole(Set<ApplicationUserPermission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public Set<ApplicationUserPermission> getPermissions() {
+        return permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities() {
+        Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+        permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+        return permissions;
+    }
+}
