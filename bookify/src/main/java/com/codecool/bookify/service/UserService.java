db425ed06a8e5375e9b3bf04184f15a6bd4b4030
Tomek K <tomek.krogulec@o2.pl>
2021-01-22 09:01:14 +0100

    commented unused code

--- bookify/src/main/java/com/codecool/bookify/service/UserService.java
+++ bookify/src/main/java/com/codecool/bookify/service/UserService.java
@@ -1,6 +1,7 @@
 package com.codecool.bookify.service;
 
 import com.codecool.bookify.exceptions.NotFoundException;
+import com.codecool.bookify.model.Role;
 import com.codecool.bookify.model.User;
 import com.codecool.bookify.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,17 +9,23 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService implements HelperService<User> {
 
     protected UserRepository userRepository;
+
+    @Autowired
+    private RoleService roleService;
+
 //    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
 
     @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+//        this.roleService = roleService;
     }
 
 //    @Autowired
@@ -45,6 +52,14 @@ public class UserService implements HelperService<User> {
         user.setId(null);
         user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
         user.setActive(true);
+//        user.getRoles().addAll(
+//                user.getRoles()
+//                        .stream()
+//                        .map(u -> {
+//                            Role role = roleService.getById(u.getId());
+//                            role.getUsers().add(user);
+//                            return role;
+//                        }).collect(Collectors.toSet()));
         userRepository.save(user);
     }
 
