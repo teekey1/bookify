12b0e9f8413931290204cc0d8322a0d1170c80b4
Tomek K <tomek.krogulec@o2.pl>
2021-01-29 03:07:02 +0100

    added swagger config

--- bookify/src/main/java/com/codecool/bookify/BookifyApplication.java
+++ bookify/src/main/java/com/codecool/bookify/BookifyApplication.java
@@ -4,9 +4,10 @@ import com.codecool.bookify.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-//@EnableJpaRepositories(basePackageClasses = UserRepository.class)
+@EnableSwagger2
 public class BookifyApplication {
 
 	public static void main(String[] args) {
